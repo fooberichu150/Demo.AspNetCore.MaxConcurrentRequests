a005b06a9972b299e3ffbd894e59f34aea947418
Jonathan Seeley <seeley.jt@gmail.com>
2018-12-19 19:45:09 -0700

    * added new demo project and class library

---
+++ MaxConcurrentRequests.Core/Middleware/MaxConcurrentRequestsMiddlewareExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MaxConcurrentRequests.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MaxConcurrentRequests.Middleware
+{
+	public static class MaxConcurrentRequestsMiddlewareExtensions
+	{
+		public static IApplicationBuilder UseMaxConcurrentRequests(this IApplicationBuilder app)
+		{
+			if (app == null)
+			{
+				throw new ArgumentNullException(nameof(app));
+			}
+
+			var throttlingSettings = app.ApplicationServices.GetService<Microsoft.Extensions.Options.IOptions<MaxConcurrentRequestsOptions>>();
+			if (!throttlingSettings?.Value.Enabled ?? false)
+				return app;
+
+			return app.UseMiddleware<MaxConcurrentRequestsMiddleware>();
+		}
+
+		public static IServiceCollection ConfigureRequestThrottleServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.Configure<MaxConcurrentRequestsOptions>(configuration.GetSection("MaxConcurrentRequests"));
+
+			return services;
+		}
+	}
+}
